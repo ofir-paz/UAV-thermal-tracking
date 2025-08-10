e608c2bb9e0cb4c0c5eb2ee1af7e7f9afdbeceb3
Ofir Paz <ophirpaz2016@gmail.com>
2025-08-10 10:54:42 +0300

    added paths for videos

---
+++ src/config.py
@@ -0,0 +1,39 @@
+from os.path import join as pjoin
+from pathlib import Path
+from typing import final
+from collections import OrderedDict
+
+
+# Paths
+ROOT_DIR = str(Path(__file__).resolve().parent.parent)
+DATA_DIR = pjoin(ROOT_DIR, "data")
+
+
+@final
+class VideosConfig:
+    """
+    Configuration class for video paths.
+    """
+    INPP = pjoin(DATA_DIR, "inpp")
+    MOVE1 = pjoin(INPP, "move1.mp4")
+    MOVING_CHECK = pjoin(INPP, "moving_check.mp4")
+    MV1 = pjoin(INPP, "mv1.mp4")
+    PERSONS = pjoin(INPP, "persons.mp4")
+    
+    VIDEOS = OrderedDict(
+        inpp=INPP,
+        move1=MOVE1,
+        moving_check=MOVING_CHECK,
+        mv1=MV1,
+        persons=PERSONS
+    )
+
+    EXAMPLE_VIDEO = MOVING_CHECK
+    TRAIN_VIDEO = MOVING_CHECK
+    
+    TEST_VIDEOS = OrderedDict(
+        inpp=INPP,
+        move1=MOVE1,
+        mv1=MV1,
+        persons=PERSONS
+    )
